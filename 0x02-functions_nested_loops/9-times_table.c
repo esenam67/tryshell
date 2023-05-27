d80dccf31143dc28d93edd22b556ee64cc42115f
esenam67 <moneyesenam@gmail.com>
2023-05-27 09:08:05 +0000

    my commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,12 +18,26 @@ for (y = 0; y <= 9; y++)
 {
 
 r = x * y;
-
+if ((r / 10) == 0)
+{
+if (y != 0)
+_putchar(' ');
 _putchar(r + '0');
+if (y == 9)
+continue;
 _putchar(',');
 _putchar(' ');
-_putchar('\n');
-
 }
+else
+{
+_putchar((r / 10) + '0');
+_putchar((r % 10) + '0');
+if (y == 9)
+continue;
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
 }
 }
