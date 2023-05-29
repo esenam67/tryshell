d2a7c879a92773f8d9c795d88b34e57ea7f2951b
esenam67 <moneyesenam@gmail.com>
2023-05-29 19:49:40 +0000

    my commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - check function
- * *n: pointer to an int
+ * @n: pointer to an int
  * Description: a function that takes a pointer to an int as
  * parameter and updates the value it points to to 98.
  *
