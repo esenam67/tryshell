5b06557a2fc723e9d0853a4dddae93ec41b775ce
esenam67 <moneyesenam@gmail.com>
2023-05-29 21:03:13 +0000

    my commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  _strlen- check function
+ * @s: pointer to a char
+ * Description:  a function that returns the length of a string.
+ *
+ * Return: nothing
+ */
+
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return (length);
+}
