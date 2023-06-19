df72b9f2ff129f4cf0adbbc1d7e32936fd5c507a
esenam67 <moneyesenam@gmail.com>
2023-06-19 16:03:46 +0000

    my commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ *  init_dog - check function
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * @d: ponter to dog
+ * Description:  a function that initialize a variable of type
+ * struct dog
+ *
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+d->name = name;
+d->age = age;
+d->owner = owner;
+
+}
