5dd021714eb252f0970bae461092f083d17017cb
esenam67 <moneyesenam@gmail.com>
2023-06-19 15:47:54 +0000

    my commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * struct my_struct - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: dog description
+ */
+
+struct dog{
+char *name;
+float age;
+char *owner;
+};
+
