be73d3c81d038da4039d86499e6ddcfc3dd833ec
esenam67 <moneyesenam@gmail.com>
2023-06-19 15:52:07 +0000

    my commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,20 @@
-#include <stdio.h>
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
- * struct my_struct - Short description
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * struct dog - struct for dog
+ * @name: name of dog
+ * @age:  it's age
+ * @owner: it's owner
  *
  * Description: dog description
  */
 
-struct dog{
+struct dog
+{
 char *name;
 float age;
 char *owner;
 };
 
+#endif /* _DOG_H_ */
