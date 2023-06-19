e651cc207e558dee76870f7449baac0b2f277567
esenam67 <moneyesenam@gmail.com>
2023-06-19 22:54:16 +0000

    my commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,12 @@ struct dog
 char *name;
 float age;
 char *owner;
-} typedef dog_t;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
