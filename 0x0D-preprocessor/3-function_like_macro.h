15d92d963dd50be7296b3f3c36cd7bea83e3e134
esenam67 <moneyesenam@gmail.com>
2023-06-19 15:14:25 +0000

    my commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,6 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
 
-#include <stdlib.h>
-#define ABS(x) abs(x)
+#define ABS(x) (((x) < 0) ?  -(x) : (x))
 
 #endif /* _FUNCTION_LIKE_MACRO_H */
