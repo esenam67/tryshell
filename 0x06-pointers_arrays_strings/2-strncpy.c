772ec92c8df39037fe6d70f660f293501fd2b6b3
esenam67 <moneyesenam@gmail.com>
2023-06-24 13:08:14 +0000

    my commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
+/**
+* *_strncpy - check code
+* @dest: pointer to char
+* @src: pointer to char
+* @n: int
+* Description: a function that concatenates two strings.
+*
+* Return: nothing
+*/
+
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
-
-      while (i < n && src[i] != '\0')
-      {
-          dest[i] = src[i];
-	  i++;
-      }
-    
-    return (dest);
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
 }
