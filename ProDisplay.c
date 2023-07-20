8d146090b9608118226d2fb1d118a51f8161ac29
esenam67 <moneyesenam@gmail.com>
2023-07-20 17:04:11 +0000

    my commit

--- ProDisplay.c
+++ ProDisplay.c
@@ -10,20 +10,35 @@
 
 int main(int ac, char **argv)
 {
-char *ThePro = "(KhaSi) $", *Entry;
+char *ThePro = "KhaSi$ ", *Entry = NULL;
 size_t TheSize = 0;
+ssize_t CheckRtr;
+/* declaring void variables so that no error of a var non used*/
+ (void)ac; (void)argv;
+/* need of infinite loop so that the $ shows again and again
+ * unless we exit */
+
+while (1)
+{
+
 /* Printing a prompt */
-write(STDOUT_FILENO, ThePro, 10);
+write(STDOUT_FILENO, ThePro, 7);
 /* print and read what is typed in */
 
-getline(&Entry, &TheSize, stdin);
-/* print out */
- printf("%s\n", Entry);
-/*write(STDOUT_FILENO, Entry, size_t TheSize);*/
+ CheckRtr = getline(&Entry, &TheSize, stdin);
+ /* check if failed or reached EOF or user use CTRL + D */
+ if (CheckRtr == -1)
+ {
+printf("Exiting shell....\n"); 
+ return -1;
+ }
+/* print out not gonna use it for now*/
+ write(STDOUT_FILENO, Entry, sizeof(Entry));
 
-/* declaring void variables */
-  (void)ac; (void)argv;
-
+/* freeing the allocated memo not created by us */
 free(Entry);
+
+}
+
 return (0);
 }
---
+++ try/main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+int main(int ac, char **argv){
+  char *prompt = "(Eshell) $ ";
+  char *lineptr;
+  size_t n = 0; 
+
+  /* declaring void variables */
+  (void)ac; (void)argv;
+
+    /* create an infinite loop */
+    while (1){
+      printf("%s", prompt);
+      getline(&lineptr, &n, stdin);
+      printf("%s\n", lineptr);
+
+      /* free up allocated memory */
+      free(lineptr);    
+    }
+
+  return (0);
+}
